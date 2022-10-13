d5ae64d010460086d6cedfd316692af92c566dbb
Breno-MT <hentgenp@gmail.com>
2022-10-13 16:34:57 -0300

    [BRENO] FEAT: Query

---
+++ pymongo_test_query.py
@@ -0,0 +1,16 @@
+from pymongo_get_database import get_database
+from pandas import DataFrame
+
+
+dbname = get_database()
+
+collection_name = dbname["user_1_items"]
+
+
+# items_details = collection_name.find()
+items_details = collection_name.find({"category" : "food"})
+
+items_df = DataFrame(items_details)
+
+print(items_df)
+
