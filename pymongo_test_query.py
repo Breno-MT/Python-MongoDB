3a96c84b8473a6b5f954f3faee58fbd045adab0f
Breno-MT <hentgenp@gmail.com>
2022-10-14 14:58:38 -0300

    [BRENO] FEAT: Function to query by category name

--- pymongo_test_query.py
+++ pymongo_test_query.py
@@ -10,9 +10,18 @@ collection_name = dbname["user_1_items"]
 # items_details = collection_name.find()
 # items_details = collection_name.find({"category" : "food"}).sort("item_name",1) # find by name asc
 # items_details = collection_name.find({"category" : "food"}).sort("item_name",-1) # find by name desc
-items_details = collection_name.find({"category" : "food"})
+# items_details = collection_name.find()
 
-items_df = DataFrame(items_details)
+# items_df = DataFrame(items_details)
 
-print(items_df)
+# print(items_df)
+
+def search_by_category(name):
+    names = collection_name.find({"category": {"$regex": f"{name}"}})
+    names_df = DataFrame(names)
+
+    if names:
+        print(names_df)
+
+search_by_category('fo2')
 
